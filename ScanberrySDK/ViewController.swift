a8cbd2e62e5cd439bc0e1d5ab5878e27468cf88d
Mister Grizzly <hi@mistergrizzly.com>
2020-08-21 13:32:37 +0300

    [Scanberry SDK] Added default Xcode Project

---
+++ ScanberrySDK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScanberrySDK
+//
+//  Created by Mister Grizzly on 8/21/20.
+//  Copyright © 2020 Mister Grizzly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
