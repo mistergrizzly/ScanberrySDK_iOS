9f01fdb672827435c2bd655eb39ace46c54c4c42
Mister Grizzly <70014641+mistergrizzly@users.noreply.github.com>
2020-08-22 02:48:10 +0300

    Update AppDelegate.swift

--- ScanberrySDK/AppDelegate.swift
+++ ScanberrySDK/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    ScanberrySDK.default.initialize(with: "YOUR_LICENSE_KEY")
     
     return true
   }
